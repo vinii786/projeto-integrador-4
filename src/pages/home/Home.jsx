707fc5860899cc007bf0d01efe7504796a7da138
vinii786 <viniciussousa@unipam.edu.br>
2023-10-30 17:13:31 -0300

    add home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,28 @@
 import "../home/Home.module.css"
+import auau from "./imgs/cao-home.png"
+import Footer from "../../components/footer/Footer"
+import styled from "../home/Home.module.css";
 
 function Home() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className={styled.Home}>
+        <Footer />
+        <div className={styled.sobre}>
+          <div className={styled.img}>
+            <img
+            src={auau}
+            alt="" 
+            />
+          </div>
+          <div className={styled.text}>
+            <h1>
+              Sobre
+            </h1>
+            <p>
+              Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
