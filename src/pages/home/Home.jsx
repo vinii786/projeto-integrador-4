9fbe03e1e3a1d6290fa2e24e1f092130a320c402
vinii786 <viniciussousa@unipam.edu.br>
2023-10-30 17:28:48 -0300

    update home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,10 +19,21 @@ function Home() {
               Sobre
             </h1>
             <p>
-              Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.
+              Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero.  Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo ddus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo ddus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti. Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo ddus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.
             </p>
           </div>
         </div>
+
+        <div className={styled.doador}>
+          <h1>
+            Qual animal pode doar?
+          </h1>
+          <h3>
+            Existem alguns pre-requisitos para que seu animal possa ser um doador! Segue a lista.
+          </h3>
+        </div>
+
+
       </div>
     );
   }
