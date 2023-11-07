23bee914bde6285dd92ce06752764c2103959949
vinii786 <viniciussousa@unipam.edu.br>
2023-11-07 17:19:45 -0300

    update home

---
+++ src/components/tabbleAnimal/tabble.jsx
@@ -0,0 +1,55 @@
+import styled from "../tabbleAnimal/tabble.module.css"; 
+import pata from "../../pages/home/imgs/pataIcon.png"
+function Tabble(){
+    return(
+            <div className={styled.animal}>
+          <h1>Cachorro</h1>
+          <div className={styled.tabble}>
+            <div className={styled.colum}>
+              <img
+                src={pata}
+                alt=""
+              />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
+            </div>
+            <div className={styled.colum}>
+              <img
+                src={pata}
+                alt=""
+              />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
+            </div>
+            <div className={styled.colum}>
+              <img
+                src={pata}
+                alt=""
+              />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
+            </div>
+            <div className={styled.colum}>
+              <img
+                src={pata}
+                alt=""
+              />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
+            </div>
+            <div className={styled.colum}>
+              <img
+                src={pata}
+                alt=""
+              />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
+            </div>
+            <div className={styled.colum}>
+              <img
+                src={pata}
+                alt=""
+              />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Tabble;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "../home/Home.module.css"
-import pata from "./imgs/pataIcon.png"
 import auau from "./imgs/cao-home.png"
 import Footer from "../../components/footer/Footer"
+import Tabble from "../../components/tabbleAnimal/tabble";
 import styled from "../home/Home.module.css";
 
 function Home() {
@@ -19,9 +19,21 @@ function Home() {
             <h1>
               Sobre
             </h1>
-            <p>
-              Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero.  Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo ddus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo ddus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti. Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo ddus necessitatibus nihil deleniti.Lorem ipatis earum doloribus recusandae am libero. Praesentium et nemo dignissimos mollitia nobis repellendus necessitatibus nihil deleniti.
-            </p>
+              <p>
+                Bem-vindo à Clínica Online de Doação de Sangue para Animais, onde o cuidado e a compaixão se encontram para ajudar nossos amigos peludos em momentos de necessidade. Nossa clínica é dedicada a fornecer uma plataforma de doação de sangue para animais que permite que os donos de animais solidários e os amantes de animais ajudem a salvar vidas e garantir o bem-estar de nossos companheiros de quatro patas.
+              </p>
+              <h3>
+                Missão e Compromisso:
+              </h3>
+              <p>
+                Nossa missão é simples, mas crucial: salvar vidas de animais por meio de doações de sangue. Comprometemo-nos a:
+              </p>
+              <ul>
+                <li>Facilitar Doações de Sangue: Oferecemos uma plataforma online fácil de usar, onde os donos de animais podem se inscrever e registrar seus animais para doação de sangue.</li><br />
+                <li>Armazenamento Seguro: Garantimos um armazenamento seguro e adequado do sangue doado para que ele esteja prontamente disponível para atender às necessidades dos animais necessitados.</li><br />
+                <li>Promover a Conscientização: Trabalhamos para aumentar a conscientização sobre a importância das doações de sangue em animais e incentivar mais pessoas a participarem.</li><br />
+                <li>Colaboração com Clínicas Veterinárias: Estabelecemos parcerias com clínicas veterinárias em todo o país para tornar o processo de doação de sangue mais acessível e conveniente.</li><br />
+              </ul>
           </div>
         </div>
 
@@ -33,54 +45,7 @@ function Home() {
             Existem alguns pre-requisitos para que seu animal possa ser um doador! Segue a lista.
           </h3>
         </div>
-
-        <div className={styled.animal}>
-          <h1>Cachorro</h1>
-          <div className={styled.tabble}>
-            <div className={styled.colum}>
-              <img
-                src={pata}
-                alt=""
-              />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
-            </div>
-            <div className={styled.colum}>
-              <img
-                src={pata}
-                alt=""
-              />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
-            </div>
-            <div className={styled.colum}>
-              <img
-                src={pata}
-                alt=""
-              />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
-            </div>
-            <div className={styled.colum}>
-              <img
-                src={pata}
-                alt=""
-              />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
-            </div>
-            <div className={styled.colum}>
-              <img
-                src={pata}
-                alt=""
-              />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
-            </div>
-            <div className={styled.colum}>
-              <img
-                src={pata}
-                alt=""
-              />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptate maxime expedita vitae doloribus veniam vero officiis saepe provident reprehenderit, aperiam ratione consequatur non quam inventore facere minus sequi obcaecati!</p>
-            </div>
-          </div>
-        </div>
+        <Tabble />
       </div>
 
     );
